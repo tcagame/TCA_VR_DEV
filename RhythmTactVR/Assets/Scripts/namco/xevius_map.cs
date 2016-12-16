bbadbc035cb4eb27ebb364055f4939d281e11c1b
ebisensupopon <ebisensupopon@gmail.com>
2016-12-17 04:02:43 +0900

    namcoステージ作業用シーン群及びprefab、material、Spriteアニメーション、テクスチャデータを追加。namcoステージ曲(ラフ2曲)を追加。

---
+++ RhythmTactVR/Assets/Scripts/namco/xevius_map.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xevius_map : MonoBehaviour {
+
+    public Vector3 scroll_spd;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += scroll_spd;
+	}
+}
